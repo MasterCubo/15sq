b918c013931aba4881490cf93cd04657a04ef1f6
Cubo <55770955+MasterCubo@users.noreply.github.com>
2024-07-23 15:09:02 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,6 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('app-cache').then(cache => {
-      return cache.addAll([
-        '/',
+const CACHE_NAME = 'app-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
         '/index.html',
         '/style.css',
         '/main.js',
@@ -11,15 +9,39 @@ self.addEventListener('install', event => {
         '/img/android-chrome-192x192.png',
         '/img/android-chrome-512x512.png',
         '/img/apple-touch-icon.png'
-      ]);
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('leaderboard.json')) { // fetch new versions of leaderboard, every time.
+    event.respondWith(fetch(event.request));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
