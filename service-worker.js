1b9633492c456d1e2bfb4bb552c713b7d2c89794
Cubo <55770955+MasterCubo@users.noreply.github.com>
2024-07-23 14:06:22 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/main.js',
+        '/sketch.js',
+        '/lb.js',
+        '/img/android-chrome-192x192.png',
+        '/img/android-chrome-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
